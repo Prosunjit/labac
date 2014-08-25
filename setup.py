df934b0502d0a49fefb98da8972ec0db6d940023
Prosunjit Biswas <prosun@Prosunjits-MacBook-Pro.local>
2014-08-24 19:06:07 -0500

    setup info (url, download url) added

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'Label Based Access Control',
   author = 'Prosunjit Biswas',
   author_email = 'prosun.csedu@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  url = 'https://github.com/prosunjit/labac', # use the URL to the github repo
+  download_url = 'https://github.com/prosunjit/labac/tarball/0.1', # I'll explain this in a second
+  keywords = ['access control', 'access control list', 'acl'], # arbitrary keywords
   classifiers = [],
 )
